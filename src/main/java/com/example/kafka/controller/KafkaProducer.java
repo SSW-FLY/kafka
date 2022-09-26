00b73d667708c6aca08791fd73d06fbc5c2ae564
yangcheng <cheng.yang@mail.hypers.com>
2022-09-26 14:24:14 +0800

    updat e

--- src/main/java/com/example/kafka/controller/KafkaProducer.java
+++ src/main/java/com/example/kafka/controller/KafkaProducer.java
@@ -1,6 +1,13 @@
 package com.example.kafka.controller;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import javax.annotation.Resource;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.TopicListing;
+import org.apache.kafka.common.KafkaFuture;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,9 +23,16 @@ public class KafkaProducer {
     @Resource
     private KafkaTemplate<Object, Object> kafkaTemplate;
 
+    @Resource
+    private AdminClient adminClient;
+
     @GetMapping("/api/kafka/normal/{message}")
     public String sendMessage(@PathVariable("message") String message) {
         kafkaTemplate.send("topic-test-llc", message);
+        ListTopicsResult result = adminClient.listTopics();
+        KafkaFuture<Set<String>> names = result.names();
+        KafkaFuture<Collection<TopicListing>> listings = result.listings();
+        KafkaFuture<Map<String, TopicListing>> future = result.namesToListings();
         return "ok";
     }
 
